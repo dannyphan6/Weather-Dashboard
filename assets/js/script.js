ad5b618df353e5ad10a615c6fe6a63c008d8b219
Daniel Phan <dannyphan@comcast.net>
2021-03-11 15:31:08 -0800

    Added content to HTML and JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
+// const searchCity
+
+
 // 3 separate fetches
 
 let weatherAPI = function() {
-    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=&units=imperial&appid=39f9546ef6f0a5b89bcb24b85f3a883a";
+    const apiURL = "https:/api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&appid=39f9546ef6f0a5b89bcb24b85f3a883a";
+    
 
     fetch(apiURL)
     .then(function(response) {
-        response.json();
+        response.json().then(function(data) {
+            console.log(data)
+            console.log(response)
+        })
     })
 }
 
 weatherAPI();
+
+$("#searchBtn").on("click", function () {
+    let searchCity = $("#search-city").val().trim();
+    localStorage.setItem(searchCity);
+})
 // hard code city name
 // get data and returning data
 // 
