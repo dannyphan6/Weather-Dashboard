cd1ef00818605c74c242af85669547c20bea7254
Daniel Phan <dannyphan@comcast.net>
2021-03-16 06:12:09 -0700

    Added second fetch into same function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
+const apiKey = "39f9546ef6f0a5b89bcb24b85f3a883a"
+
 let weatherAPI = function () {
-    let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&appid=39f9546ef6f0a5b89bcb24b85f3a883a";
+    let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Seattle&units=imperial&appid=" + apiKey;
 
     fetch(apiURL)
         .then(function (response) {
             response.json().then(function (data) {
-                console.log(response)
-                console.log(data)
+                console.log(response);
+                console.log(data);
 
                 let currentCity = data.name;
                 console.log(currentCity);
                 
                 $("#current-city").text("Name of City: " + currentCity);
-                let currentCityImg = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png")
-                $("#current-city").append(currentCityImg)
+                let currentCityImg = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+                $("#current-city").append(currentCityImg);
 
                 let currentTemp = data.main.temp;
                 console.log(currentTemp);
@@ -28,13 +30,29 @@ let weatherAPI = function () {
                 let currentWindSpeed = data.wind.speed;
                 console.log(currentWindSpeed);
                 $("#current-wind-speed").text("Wind Speed: " + currentWindSpeed + " MPH");
+
+                let currentLat = data.coord.lat
+                let currentLon = data.coord.lon
+
+                const secondApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currentLat + "&lon=" + currentLon + "&exclude=hourly,daily&appid=" + apiKey;
+
+                fetch(secondApi)
+                .then(function(response) {
+                    response.json().then(function(data) {
+                        console.log(response);
+                        console.log(data);
+                        let uvIndex = data.current.uvi;
+                        console.log(uvIndex);
+                        $("#uv-index").text("UV Index: " + uvIndex);
+                    })
+                })
             })
         })
-}
+};
 
 let index = 1;
-let secondFetch = function () {
-    let apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=Seattle&units=imperial&appid=39f9546ef6f0a5b89bcb24b85f3a883a"
+let fiveDayWeather = function () {
+    let apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=Seattle&units=imperial&appid=" + apiKey;
 
     fetch(apiURL)
         .then(function (response) {
@@ -48,49 +66,37 @@ let secondFetch = function () {
                     if (data.list[i].dt_txt.indexOf("00:00:00") !== -1) {
                         console.log(data.list[i]);
                         let fiveDayDate = new Date(data.list[i].dt_txt).toLocaleDateString();
-                        let pTagOne = $("<p>").text(fiveDayDate);
+                        let pTagOne = $("<h4>").text(fiveDayDate);
                         console.log(pTagOne);
+                        // let fiveDayDateImg = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.list[i])
 
                         let fiveDayTemp = data.list[i].main.temp;
-                        let pTagTwo = $("<p>").text(fiveDayTemp)
+                        let pTagTwo = $("<p>").text("Temperature: " + Math.round(fiveDayTemp) + " °F");
                         console.log(pTagTwo);
 
                         let fiveDayHumidity = data.list[i].main.humidity;
-                        let pTagThree = $("<p>").text(fiveDayHumidity)
+                        let pTagThree = $("<p>").text("Humidity: " + fiveDayHumidity + " %");
                         console.log(pTagThree);
 
-                        $(`#Day-${index}`).append(pTagOne)
+                        $(`#Day-${index}`).append(pTagOne);
                         $(`#Day-${index}`).append(pTagTwo);
-                        $(`#Day-${index}`).append(fiveDayHumidity)
+                        $(`#Day-${index}`).append(fiveDayHumidity);
                         index++;
                     }
                 }
             })
         })
-}
-
-let thirdFetch = function () {
-    let apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=47.608013&lon=-122.335167&exclude=current,daily,alerts&appid=39f9546ef6f0a5b89bcb24b85f3a883a"
-
-    fetch(apiURL)
-        .then(function (response) {
-            response.json().then(function (data) {
-                console.log(data)
-                console.log(response)
-                let uvIndex = data.hourly[0].uvi
-                console.log(uvIndex)
-                $("#uv-index").text("UV Index: " + uvIndex);
-            })
-        })
-}
+};
 
 weatherAPI();
-secondFetch();
-thirdFetch();
+fiveDayWeather();
+
 
 $("#searchBtn").on("click", function () {
     let searchCity = $("#search-city").val().trim();
-    let saveCityLocation = localStorage.setItem("City", searchCity);
+    localStorage.setItem("City", searchCity);
+    weatherAPI();
+    fiveDayWeather();
 
 })
 // hard code city name
